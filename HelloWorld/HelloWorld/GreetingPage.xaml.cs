71a98b01892e1e340440516fbee07d4e57fbe227
mlstephens <mlstephens@earthlink.net>
2019-03-03 15:31:41 -0800

    Add project files.

---
+++ HelloWorld/HelloWorld/GreetingPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class GreetingPage : ContentPage
+	{
+		public GreetingPage ()
+		{
+			InitializeComponent();
+            slider.Value = 0.5;
+
+            // how to set device related code etc. this sets padding
+            //Padding = Device.OnPlatform(
+            //    iOS: new Thickness(0, 20, 0, 0),
+            //    Android: new Thickness(0, 50, 0, 0),
+            //    WinPhone: new Thickness(0, 10, 0, 0)
+            //);
+        }
+    }
+}
