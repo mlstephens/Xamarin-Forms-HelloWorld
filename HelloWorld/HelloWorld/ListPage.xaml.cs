4e3c213777425fff02468b16556330488a3fb6e6
mlstephens <mlstephens@earthlink.net>
2019-07-07 13:02:13 -0700

    list view with selected item and tapped item.

--- HelloWorld/HelloWorld/ListPage.xaml.cs
+++ HelloWorld/HelloWorld/ListPage.xaml.cs
@@ -18,25 +18,36 @@ namespace HelloWorld
 			InitializeComponent ();
 
             //simple lists
-            //listView.ItemsSource = new List<Contact>
-            //{
-            //    new Contact{ Name="Anna", ImageUrl="http://lorempixel.com/100/100/people/1" },
-            //    new Contact{ Name="Mosh", Status="Text me please.", ImageUrl="http://lorempixel.com/100/100/people/3" }
-            //};
+            listView.ItemsSource = new List<Contact>
+            {
+                new Contact{ Name="Anna", ImageUrl="http://lorempixel.com/100/100/people/1" },
+                new Contact{ Name="Mosh", Status="Text me please.", ImageUrl="http://lorempixel.com/100/100/people/3" }
+            };
 
             //grouping
-            listView.ItemsSource = new List<ContactGroup>
-            {
-                new ContactGroup("A","A"){
-                    new Contact{ Name="Anna", ImageUrl="http://lorempixel.com/100/100/people/1" },
-                },
-                
-                new ContactGroup("M","M")
-                {
-                    new Contact{ Name="Mosh", Status="Text me please.", ImageUrl="http://lorempixel.com/100/100/people/3" }
-                }
-                
-            };
+            //listView.ItemsSource = new List<ContactGroup>
+            //{
+            //    new ContactGroup("A","A"){
+            //        new Contact{ Name="Anna", ImageUrl="http://lorempixel.com/100/100/people/1" },
+            //    },
+
+            //    new ContactGroup("M","M")
+            //    {
+            //        new Contact{ Name="Mosh", Status="Text me please.", ImageUrl="http://lorempixel.com/100/100/people/3" }
+            //    }
+            //};
         }
-	}
+
+        private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var contact = e.Item as Contact;
+            DisplayAlert("Tapped:", contact.Name, "OK");
+        }
+
+        private void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var contact = e.SelectedItem as Contact;
+            DisplayAlert("Selected:", contact.Name, "OK");
+        }
+    }
 }
